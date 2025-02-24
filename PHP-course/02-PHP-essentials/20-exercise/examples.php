743d6604bc9dafa5b33612f0260bc6d914b8a907
bvnderb <brent.vd.bruggen@gmail.com>
2025-02-24 19:48:03 +0100

    checked exercise as completed

--- PHP-course/02-PHP-essentials/20-exercise/examples.php
+++ PHP-course/02-PHP-essentials/20-exercise/examples.php
@@ -15,7 +15,7 @@ $isRed = false;
 
     <?php 
 // exercise: => check if empty and use if else endif 
-// we want the page name to be displayed in the browser (on the tab itself) and if no name can't be found, set a default (use the $pageTitle in header.inc) 
+// we want the page name to be displayed in the browser (on the tab itself) and if no name can't be found, set a default (use the $pageTitle in header.inc) => OK!
 // provide a default background img in case the header inc doesn't work for some reason (use the $headImg for this)
 // indicate which page is active (highlight with css class="active") use the $pageKey for this
 
