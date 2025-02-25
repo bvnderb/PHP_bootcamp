88a79382d9b9c1b5e47c5a939c60ea045bcd724f
bvnderb <brent.vd.bruggen@gmail.com>
2025-02-25 21:13:22 +0100

    renamed folder from 03 - arrays to 03-arrays

---
+++ PHP-course/03-arrays/exercises/playlist/playlist.php
@@ -0,0 +1,7 @@
+<?php
+
+$playList = ['Starry Night', 'Moonlit Walk', 'Whispering Wind', 'Golden Horizon'];
+
+if (!isset($playList) || empty($playList)) {
+    echo "Your playlist needs an update. Time to discover more music!";
+}
