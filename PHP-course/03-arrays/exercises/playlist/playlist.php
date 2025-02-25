0058d7625a1e234687c963e606c5f3eb4ca57048
bvnderb <brent.vd.bruggen@gmail.com>
2025-02-25 21:36:33 +0100

    exercise works

--- PHP-course/03-arrays/exercises/playlist/playlist.php
+++ PHP-course/03-arrays/exercises/playlist/playlist.php
@@ -1,7 +1,13 @@
 <?php
 
-$playList = ['Starry Night', 'Moonlit Walk', 'Whispering Wind', 'Golden Horizon'];
+$playlist = ['Lunar Echoes', 'Solar Whispers', 'Sunny Days', 'Aurora'];
+$num = count($playlist);
 
-if (!isset($playList) || empty($playList)) {
+if (!isset($playlist) || empty($playlist)) {
     echo "Your playlist needs an update. Time to discover more music!";
-}
+} if (in_array("Sunny Days", $playlist)) {
+    echo "You have great taste! 'Sunny Days' always lifts the mood!";
+} elseif ($num -1 >= 2) {
+    $playlist[1] = "Solar Whispers";
+    echo "We have added {$playlist[1]} to your list!";
+} 
