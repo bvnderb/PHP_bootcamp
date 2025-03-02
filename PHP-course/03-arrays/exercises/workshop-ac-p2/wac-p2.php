c4ea112018aa5e55d9b252e9bd73e8793fad392c
bvnderb <brent.vd.bruggen@gmail.com>
2025-03-02 16:55:50 +0100

    will continue tomorrow

--- PHP-course/03-arrays/exercises/workshop-ac-p2/wac-p2.php
+++ PHP-course/03-arrays/exercises/workshop-ac-p2/wac-p2.php
@@ -2,24 +2,64 @@
 
 // provided info
 $waitingList = [
-    'Alex Reed',
-    'Blake Jordan',
-    'Casey Smith',
-    'Drew Alex',
-    'Elliot Ford',
-    'Finley Harper',
-    'Jordan Kay',
-    'Kim Lee',
-    'Liam Park',
-    'Morgan Drew'
+'Eva Grant', 'Ian Hope', 'Olivia Jane'
 ];
 
 $priorityParticipants = [
-    'Jordan Kay', // In the waiting list and has priority
-    'Sam Taylor', // Not in the waiting list but has priority
-    'Zane Pryor'  // Not in the waiting list but has priority
+    
 ];
 
 $individualName = 'Alex Reed';
 
 // my code
+$finalAttendees = [];
+$backupCandidates = [];
+
+foreach ($priorityParticipants AS $participant) {
+   // echo "Checking priority participant: $participant\n"; // debugging
+    if (count($finalAttendees) < 5) {
+        if (!in_array($participant, $finalAttendees)) {
+            $finalAttendees[] = $participant;
+           // echo "Adding to finalAttendees: $participant\n"; // debugging
+        } else {
+            if (!in_array($participant, $backupCandidates) && (!in_array($participant, $finalAttendees))) {
+                $backupCandidates[] = $participant;
+                // echo "Adding to backupCandidates: $participant\n";
+            }
+            
+        }
+    } 
+}  
+
+foreach ($waitingList AS $waiting) {
+    // echo "Checking waiting participant: $waiting\n"; // debugging
+    if (count($finalAttendees) < 5) {
+        if (!in_array($waiting, $finalAttendees)) {
+            // echo "Adding to finalAttendees: $waiting\n"; // debugging
+            $finalAttendees[] = $waiting;
+        }
+    } else {
+        if (!in_array($waiting, $backupCandidates) && (!in_array($waiting, $finalAttendees))) {
+            // echo "Adding to backupCandidates: $waiting\n";
+            $backupCandidates[] = $waiting;
+        }
+        
+    }
+}
+
+$backup = array_slice($backupCandidates, 0, 3);
+
+foreach ($backup AS $candidate) {
+    $individualName = $candidate;
+    echo "Hey {$individualName}, we want to inform you that you are one of our backup candidates. 🥳";
+}
+
+sort($finalAttendees);
+sort($backupCandidates);
+
+// testing the code
+echo "Final Attendees:\n";
+print_r($finalAttendees);
+
+echo "Backup Attendees:\n";
+print_r($backupCandidates);
